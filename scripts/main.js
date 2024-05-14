6d725eaef07d2d0f7d0e16453ea8f9049df32030
yuyeol3 <102399989+yuyeol3@users.noreply.github.com>
2024-05-14 17:23:57 +0900

    Update main.js
    clean up the code

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,6 @@
 import { world } from "@minecraft/server";
 import { startAfterPlayerDeath } from "./deadPlayerItemHandler"
 
-
-
 world.afterEvents.entityDie.subscribe( startAfterPlayerDeath );
 world.getDimension("minecraft:overworld").runCommandAsync("/gamerule keepinventory true");
 world.getDimension("minecraft:nether").runCommandAsync("/gamerule keepinventory true");
